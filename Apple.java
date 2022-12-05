ffcf8ba6d13f5bd63c57b91f96f09e5b4b904ebf
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-05 14:07:09 -0500

    Add game over

--- Apple.java
+++ Apple.java
@@ -2,12 +2,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Apple extends Actor
 {
-
-    
     public void act()
     {
         int x = getX();
         int y = getY() + 2;
         setLocation(x, y);
+        
+        int bottomY = getWorld().getHeight();
+        if(getY() > bottomY) {
+            MyWorld world = (MyWorld) getWorld();
+            world.gameOver();
+            world.removeObject(this);
+        }
     }
 }
