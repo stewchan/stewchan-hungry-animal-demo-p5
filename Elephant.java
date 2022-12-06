520610e65a834a5cc7069e960d6863fdaf659cbe
Stewart Chan <stewart.chan@gapps.yrdsb.ca>
2022-12-06 13:52:48 -0500

    Add sound and titlescreen

--- Elephant.java
+++ Elephant.java
@@ -2,6 +2,12 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
 {
+    GreenfootSound sound;
+    
+    public Elephant(){
+        sound = new GreenfootSound("sounds/elephantcub.mp3");
+    }
+    
     public void act()
     {
         if(Greenfoot.isKeyDown("d")){
@@ -16,6 +22,8 @@ public class Elephant extends Actor
     
     public void eat(){
         if(isTouching(Apple.class)){
+            // play sound
+            sound.play();
             removeTouching(Apple.class);
             
             MyWorld world = (MyWorld) getWorld();
